c52bc72baaf9e64b7317912ad5e253e63d607dab
Colin Cramer <colin.w.cramer@gmail.com>
2020-08-27 22:37:56 -0700

    Week 11 Homework

--- src/dao/DBConnection.java
+++ src/dao/DBConnection.java
@@ -8,7 +8,7 @@ public class DBConnection {
 	
 	private final static String URL = "jdbc:mysql://localhost:3306/pizza";
 	private final static String USERNAME = "root";
-	private final static String PASSWORD = "Cr4m3r_D4wg";
+	private final static String PASSWORD = "root";
 	private static Connection connection;
 	private static DBConnection instance;
 	
